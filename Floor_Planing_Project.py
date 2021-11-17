f8bdf4766e727cdd04ebfcd191cccb64975f9550
mhdahab <67865770+mhdahab@users.noreply.github.com>
2021-11-17 19:23:16 +0200

    Add files via upload

--- Floor_Planing_Project.py
+++ Floor_Planing_Project.py
@@ -48,20 +48,43 @@ def calculate_cell_area(listOfComponents,LEF_file_name):
                 if(found):
                    lef_file.close()
                    break
-    return cells_area
+    return (cells_area*1000)
+  
 
-def calculatewidth( core_area , aspect_ratio):
-    width =math.sqrt(core_area/aspect_ratio)
-    if(width%0.46!=0):
-        width=math.ceil(width/0.46)*0.46
+def calculatewidth(core_area , aspect_ratio , w_margin ,h_margin ):
+    a = aspect_ratio 
+    b= (aspect_ratio * w_margin)-h_margin 
+    c= core_area *-1
+    discriminant = (b*b)-(4.0*a*c)
+
+    width =((-1.0*b) - math.sqrt(discriminant))/(2.0*a)
+    if width < 0.0 :
+         width =((-1.0*b) + math.sqrt(discriminant))/(2.0*a)
+    if(width%(0.46*1000)!=0.0):
+        width=int(round(width/(0.46*1000))*(0.46*1000))
     return width
 
-def calculateheight(die_area_width , aspect_ratio) :
-    height=aspect_ratio*die_area_width
-    if(height%2.72!=0):
-        height=math.ceil(height/2.72)*2.72
-    return height
+def calculateheight(aspect_ratio,die_area_width,h_margin) :
+    h=(aspect_ratio*die_area_width)-h_margin
+    if(h%(2.72*1000)!=0.0):
+        h=int(round(h/(2.72*1000))*(2.72*1000))
 
+    return h
+
+def printROW(outfile,die_area_height,die_area_width ,w_margin ,h_margin):
+ i=int((die_area_height-(h_margin))/(2.72*1000))
+ i_org=i
+ orgiX=int(w_margin/2)
+ orgiY=int(h_margin/2)
+ step=int(0.46*1000)
+ numX =int((die_area_width - w_margin)/step)
+ 
+ while i>=0 :
+     outfile.write("ROW ROW_"+str(i_org-i)+ " unithd " + str(orgiX)+" "+ str(orgiY) +" N DO "+ str(numX) +" BY 1 STEP "+str(step) +" 0 ;"+'\n') 
+     orgiY+=int((2.72*1000))
+     outfile.write("ROW ROW_"+str(i_org-i+1)+" unithd " + str(orgiX)+" "+ str(orgiY) +" FS DO "+ str(numX) +" BY 1 STEP "+str(step) +" 0 ;"+'\n')
+     orgiY+=int((2.72*1000))
+     i-=2
 
 def printComponents(outputfile,components_number,listOfComponents):
     outputfile.write("Components " + str(components_number) +";\n")
@@ -86,9 +109,9 @@ def main():
    LEF_file_name=input("Please Enter the  library LEF file name \n")
    #pinLoaction_file_name=input("Please Enter the pin location file name \n")
    core_utilization=float(input("Please Enter the core area utilization percentage \n"))
-   aspect_ratio=float(input("Please Enter the core area aspect_ratio \n"))
-   h_margin=float(input("Please Enter the vertical margin \n"))
-   w_margin=float(input("Please Enter the horizonal margin \n"))
+   aspect_ratio=float(input("Please Enter the aspect ratio \n"))
+   h_margin=2*float(input("Please Enter the vertical margin \n"))
+   w_margin=2*float(input("Please Enter the horizonal margin \n"))
 
   
 
@@ -99,8 +122,8 @@ def main():
 
    cells_area=calculate_cell_area(listOfComponents,LEF_file_name)
    core_area =cells_area/core_utilization
-   die_area_width= calculatewidth(core_area , aspect_ratio) +(2*w_margin)
-   die_area_height= calculateheight(die_area_width , aspect_ratio) +(2*h_margin)
+   die_area_width= int(calculatewidth(core_area , aspect_ratio , w_margin ,h_margin ) +(w_margin))
+   die_area_height= int(calculateheight(aspect_ratio,die_area_width,h_margin) +(h_margin))
 
    outputfile.write("VERSION 5.8;" +'\n')
    outputfile.write("DIVIDERCHAR \"/\" ;" +'\n')
@@ -109,6 +132,7 @@ def main():
    outputfile.write("UNITS DISTANCE MICRONS 1000;" +'\n')
    outputfile.write("DIEAREA ( 0 0 ) ("+ str(die_area_width) + ","+ str(die_area_height) +");" +'\n')
 
+   printROW(outputfile,die_area_height,die_area_width ,w_margin ,h_margin)
    printComponents(outputfile,components_number,listOfComponents)
    
 
